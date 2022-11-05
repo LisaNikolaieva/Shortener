7cd41614792dd5f955494f5c4d9c0430ce7a9d29
Yelyzaveta_Nikolaieva <100728186+LisaNikolaieva@users.noreply.github.com>
2022-11-05 17:22:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from aiohttp import web
 from db import setup_db
 from bson import ObjectId
-
+import re
 
 async def shut_url_get(request):
     form_request = """
@@ -23,9 +23,10 @@ async def shut_url_get(request):
 async def shut_url_post(request):
     result = await request.text()
     user_url = result.replace('user_url=', '')
+    user_url_list = user_url.split('://') #re.sub(r"https?://", '', user_url)
     db = request.app["db"]
     collection = db['shortener']
-    url_record = await collection.insert_one({'user_url': user_url})
+    url_record = await collection.insert_one({'user_url': user_url_list[1], 'prefix': user_url_list[0]})
     return web.Response(text=str(url_record.inserted_id))
 
 
@@ -33,9 +34,10 @@ async def handle(request):
     name_url = request.match_info.get('name')
     db = request.app["db"]
     collection = db['shortener']
-    find_url = await collection.find_one({"_id":  ObjectId(name_url)})
-    select_url = find_url['user_url']
-    return web.HTTPFound('http://' + select_url)
+    obj_url = await collection.find_one({"_id":  ObjectId(name_url)})
+    prefix = obj_url.get('prefix', 'http')
+    url = obj_url['user_url']
+    return web.HTTPFound(prefix + '://' + url)
 
 
 db = asyncio.run(setup_db())
